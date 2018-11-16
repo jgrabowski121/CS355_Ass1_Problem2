5c2ef51f68e1a822b770c862a26e03f268561e80
Justin Grabowski <justingrabowski@yahoo.com>
2018-11-16 12:39:04 -0500

    Initial Commit

---
+++ CS355_Ass1_Problem2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS355_Ass1_Problem2
+//
+//  Created by Justin Grabowski on 11/16/18.
+//  Copyright © 2018 Justin Grabowski. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
